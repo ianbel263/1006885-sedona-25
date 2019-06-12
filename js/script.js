9ee9adeb72ce63b6038a175c6535bf2444ea3d9e
Andrey Belyaev <ianbel263@gmail.com>
2019-06-12 10:01:39 +0300

    Иправлены ошибки

--- js/script.js
+++ js/script.js
@@ -4,17 +4,26 @@ var form = popup.querySelector('.modal-booking-form');
 var dateArrival = popup.querySelector('[name=arrival-date]');
 var dateDeparture = popup.querySelector('[name=departure-date]');
 
+popup.classList.add('modal-close');
+
 btnModal.addEventListener('click', function(evt) {
 	evt.preventDefault();
-	popup.classList.toggle('modal-show');
+	if (popup.classList.contains('modal-close')) {
+		popup.classList.remove('modal-close');
+		popup.classList.add('modal-open');
+	} else {
+		popup.classList.add('modal-close');
+		popup.classList.remove('modal-open');
+	}
 });
 
 window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
 			evt.preventDefault();
-			if (popup.classList.contains('modal-show')) {
-				popup.classList.remove('modal-show');
+			if (!popup.classList.contains('modal-close')) {
+				popup.classList.add('modal-close');
 				popup.classList.remove('modal-error');
+				popup.classList.remove('modal-open');
 			}
   }
 });
